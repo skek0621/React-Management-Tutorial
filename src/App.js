4ec245c26dc0b96b3bab1bb2d80af6a4bf1f824d
hyunmin cha <hyunmincha@hyunminui-MacBookPro.local>
2020-03-25 13:20:08 +0900

    Create  Customer.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,57 @@
 import React from 'react';
 import logo from './logo.svg';
+import Customer from './components/Customer'
 import './App.css';
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2> Let's develop management system</h2>
-    </div>
-  );
+const customers = [
+  {
+  'id' : 1,
+  'image': 'https://placeimg.com/64/64/any',
+  'name' : '차현민',
+  'birthday' : '950621',
+  'gender' : '여자',
+  'job' : '직장인'
+  },
+  {
+  'id' : 2,
+  'image': 'https://placeimg.com/64/64/1',
+  'name' : '정현우',
+  'birthday' : '870301',
+  'gender' : '남자',
+  'job' : '공무원'
+  },
+  {
+    'id' : 3,
+    'image': 'https://placeimg.com/64/64/2',
+    'name' : '정지원',
+    'birthday' : '930306',
+    'gender' : '여자',
+    'job' : '직장인'
+  }
+]
+
+class App extends React.Component {
+  render(){
+      return(
+        <div>
+          {
+            customers.map(c=>{
+              return(
+                <Customer
+                  key={c.id}
+                  id={c.id}
+                  name={c.name}
+                  image={c.image}
+                  gender={c.gender}
+                  birthday={c.birthday}
+                  job={c.job}
+                  />
+              );
+            })
+          }
+        </div>
+      );
+  }
 }
 
 export default App;
